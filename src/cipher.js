4b08044db455038e30fb201e98fe48b3ba88da11
paulagodoyh5 <paula.gh5@gmail.com>
2019-08-25 01:03:29 -0400

    Cifra, descifra, hace espacio, falta css y readme

--- src/cipher.js
+++ src/cipher.js
@@ -3,29 +3,40 @@ window.cipher = {
   encode: (message, offset) => {
       let result="";
       let ascii= "";
-      let espacio="";
-      console.log(message)
+      let character = "";
+    
     for (let i = 0; i < message.length ; i++) {
-      ascii = ((message.charCodeAt(i) - 65 + offset)%26 + 65);
-      console.log(ascii)
-      ascii = String.fromCharCode(ascii);
-      console.log(ascii)
-      result += ascii;
- 
-    /*if (ascii === 32) {
-        result = String.fromCharCode(32);
-    }*/ 
-  }return ascii;
+       character = message[i].charCodeAt();
+      
+      if(character===32){
+        ascii = String.fromCharCode(32);
+        result+= ascii;
+
+      }else{
+        let ascii = ((message.charCodeAt(i) - 65 + offset)%26 + 65);
+        ascii = String.fromCharCode(ascii);
+        result += ascii;
+      } 
+  }return result;
 },
   decode: (message, offset) => {
 
      let resultDecode="";
+     let ascii= "";
+     let character= "";
 
-  for (let i = 0; i < message.length ; i++) {
+    for (let i = 0; i < message.length ; i++) {
+      character = message[i].charCodeAt();
 
-    let ascii = ((message.charCodeAt(i) - 90 - offset)%26 + 90);
-    ascii = String.fromCharCode(ascii);
-    resultDecode += ascii;
+    if(character===32){
+        ascii = String.fromCharCode(32);
+        resultDecode += ascii;
+
+    }else{
+      let ascii = ((message.charCodeAt(i) - 90 - offset)%26 + 90);
+      ascii = String.fromCharCode(ascii);
+      resultDecode += ascii;
+    }
     
-    }return resultDecode;}
-  }; 
+    }return resultDecode;
+  }}; 
